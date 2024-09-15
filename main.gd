4ac6dcd47af44513446270d16141a8586a804313
xXTitouanXx <17titou@gmail.com>
2024-09-15 18:50:07 +0200

    Finish

--- main.gd
+++ main.gd
@@ -3,23 +3,20 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	new_game()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func game_over() -> void:
+	$Music.stop()
+	$GameOverSound.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game() -> void:
+	$Music.play()
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("GET READY!")
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
@@ -29,6 +26,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout() -> void:
